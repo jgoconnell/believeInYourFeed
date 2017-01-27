09f03f0a2bd3cf07845c0fb35c413dbdb3ca30c1
John O'Connell <John@Johns-MacBook-Pro.local>
2017-01-27 01:06:34 +0100

    Adding rough websockets version with tags and batteries

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # believeInYourFeed
 Arduino Yún using Python's Tweepy to change stuff
+
+### Install required packages:
+
+e.g. with pip
+
+```
+pip install tweepy gevent-websocket
+```
+
+### Run server
+```
+./websockets_server.py
+```
+
+Once the server is running you can open the test_client.html to test it
+
---
+++ websocket_server.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+import time
+import json
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+
+class HashTagCount(object):
+    def __init__(self):
+        self.charge_threshold_secs = 2.0
+        self.max_charge = 100.0
+        self.max_charge_tweets = 10.0
+        self.increment_unit = self.max_charge / self.max_charge_tweets 
+        self.decrement = self.increment_unit / 10
+
+        self.last_updated = time.time()
+        self.last_charge_level_decay_poll = 0
+
+        self.charge_level = 0.0
+        self.count = 0
+
+    def increment(self):
+        time_now = time.time()
+        time_diff = time_now - self.last_updated
+        self.last_updated = time_now
+
+        if self.charge_level < self.max_charge:
+            self.charge_level += self.increment_unit 
+        if self.charge_level > self.max_charge:
+            self.charge_level = self.max_charge 
+
+        self.last_charge_level_decay_poll = 0
+        self.count += 1
+
+    def get_value(self):
+        time_diff = time.time() - self.last_updated
+        if time_diff <= self.charge_threshold_secs:
+            return self.charge_level
+        else: 
+            decay_diff = 0
+            if self.last_charge_level_decay_poll != 0:
+                decay_diff = time.time() - self.last_charge_level_decay_poll
+            else:
+                decay_diff = time.time() - self.last_updated
+            self.last_charge_level_decay_poll = time.time()
+            decay_to_apply = decay_diff * self.decrement
+            if self.charge_level > 0:
+                self.charge_level -= decay_to_apply
+            else:
+                self.charge_level = 0.0
+            if self.charge_level < 0.0:
+                self.charge_level = 0.0
+        return self.charge_level
+
+
+class HashTagCounter(object):
+    def __init__(self):
+        self.num_clients = 0
+        self.hash_tags = ["#starwars", "#trump", "#bestfanarmy", "#apple", "#google", "#love", "#selfie"]
+        self.hashtag_index = 0;
+        self.hashtag_map = self.get_tag_counter()
+
+    def add_user(self):
+        self.hashtag_index = self.num_clients % len(self.hash_tags)
+        self.num_clients = self.num_clients + 1
+        return self.hash_tags[self.hashtag_index]
+
+    def get_tag_counter(self):
+        return dict([(tag, HashTagCount()) for tag in self.hash_tags])
+    
+
+class Settings(object):
+    consumer_key = "pr13t2gmj8jFo0Har8CvUSt9Z"
+    consumer_secret = "ZT9X2ZJYw2Wlr4D9L84g7vMVRUB1humUSa2cy3W352gC7QCVSG"
+    access_token="2190622657-MU2mA5QNqCwjoR7pqzt4nmSaKyRqNcDe0cAjGCc"
+    access_token_secret="7LnuWchuwEbsliDRJeBMGRioBx2gFtfeqgJwXxZR0wCzF"
+    server_port = 6699
+    max_clients = 7
+    client_poll_interval = 0.5
+    KILL_TWEEPY = False
+
+
+settings = Settings()
+tag_counter = HashTagCounter()
+
+class TestTweetListener(StreamListener):
+    """ A listener handles tweets that are received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_status(self, status):
+        print("Receieve a tweet from twitter and update the values in a global array? %s" % status)
+    def on_data(self, data):
+        #print("Got data! %s" % data)
+        if settings.KILL_TWEEPY:
+            return False
+        else:
+            #import pdb; pdb.set_trace()
+            json_data = json.loads(data)
+            if "text" in json_data:
+                for tag in tag_counter.hashtag_map.keys():
+                    if tag.lower() in json_data["text"].lower():
+                        #print("Incrementing count for tag: %s" % tag)
+                        tag_counter.hashtag_map[tag].increment()
+            #tag_counter.hashtag_map[tag]
+            return True
+
+    def on_error(self, status):
+        print("Received an error: %s" % status)
+        return True
+
+    def on_timeout(self):
+        print('Receieved a Timeout from the twitter data service')
+        return True
+
+    def on_disconnect(self, notice):
+        """Called when twitter sends a disconnect notice
+   
+        Disconnect codes are listed here:
+        https://dev.twitter.com/docs/streaming-apis/messages#Disconnect_messages_disconnect
+        """
+        print(" Received a  on_disconnect")
+        return True
+
+
+class ClientDataPusher(object):
+    def __init__(self, ws, tag):
+        self.tag = tag
+        self.ws = ws
+        self.start_pushing()
+
+    def start_pushing(self):
+        self.ws.send(self.tag)
+        while True:
+            time.sleep(settings.client_poll_interval) 
+            print("Wake up and send the count to our client")
+            data_for_client = {}
+            data_for_client["charge_level"] = tag_counter.hashtag_map[self.tag].get_value()
+            data_for_client["total_count"] = tag_counter.hashtag_map[self.tag].count
+            data_for_client["last_updated"] = tag_counter.hashtag_map[self.tag].last_updated
+            data_for_client["tag_name"] = self.tag
+            print ("Sending data to client: %s" % data_for_client)
+            self.ws.send("%s" % json.dumps(data_for_client))
+
+
+client_data_pushers = []
+
+
+def app(environ, start_response):
+    ws = environ['wsgi.websocket']
+    data = ws.receive()
+    if len(client_data_pushers) < settings.max_clients:
+        tag = tag_counter.add_user()
+        print ("Got a new connectiion on this tag: %s" % tag)
+        client_data_pushers.append(ClientDataPusher(ws, tag))
+    else:
+        ws.close()
+
+
+if __name__ == "__main__":
+
+    l = TestTweetListener()
+    auth = OAuthHandler(Settings.consumer_key, Settings.consumer_secret)
+    auth.set_access_token(Settings.access_token, Settings.access_token_secret)
+
+    stream = Stream(auth, l, timeout=40000)
+    stream.filter(track=tag_counter.hash_tags, async=True)
+    try:
+        print("Serving starting up listenening on port: %d" % settings.server_port)
+        server = WSGIServer(("", settings.server_port), app,handler_class=WebSocketHandler)
+        server.serve_forever()
+    except KeyboardInterrupt, e:
+        print("Receieved Ctrl-C, disconnect the tweepy")
+        stream.disconnect()
+    except Exception, e:
+        print("Unknown exception, byebye : %s" % e)
+    settings.KILL_TWEEPY = True
